45f06756ee3381bf0c92ea86185a2b34c987c8a2
William Amory <48212125+w2994a@users.noreply.github.com>
2021-09-19 16:14:00 +0200

    Delete TP1 directory

--- TP1/perc_GC.py
+++
@@ -1,6 +0,0 @@
-# Ex 3.6.5
-perc_GC = ((4500 + 2575 )/14800)*100
-print(f"Le pourcentage de CG est {perc_GC:<6.0f} %")
-print(f"Le pourcentage de CG est {perc_GC:<6.1f} %")
-print(f"Le pourcentage de CG est {perc_GC:<6.2f} %")
-print(f"Le pourcentage de CG est {perc_GC:<6.3f} %")
