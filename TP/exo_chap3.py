7c69a819cb3df8737fdc1decc2df9ad506594606
William AMORY <w_amory@orange.fr>
2021-09-19 16:12:52 +0200

    reorganization of directories

---
+++ TP/exo_chap3.py
@@ -0,0 +1,15 @@
+# Exercice chapitre 3
+
+# Ex 3.6.1
+print("A" * 20)
+
+# Ex 3.6.2
+print("A" * 20 + "G" * 40)
+
+# Ex 3.6.3
+a = "salut"
+b = 102
+c = 10.318
+print(f"a = {a} ; b = {b} ; c = {c:.2f}")
+
+# Ex 3.6.4
